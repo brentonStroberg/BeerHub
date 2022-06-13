9e26dcbb12b6502caf87f44ed27b456c6e858552
Raees Aziz Muhammad <raees@bbd.co.za>
2022-06-13 11:59:55 +0200

    Add database connection

---
+++ BeerHub/Database/DBManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeerHub.Database
+{
+  public class DBManager : DbContext
+  {
+    public DBManager(DbContextOptions<DBManager> options) : base(options)
+    {
+    }
+  }
+}
