9108b7376c5eb0aa080f7d62d4af4b4287306a27
Raees Aziz Muhammad <raees@bbd.co.za>
2022-06-13 22:32:23 +0200

    Rest Http error models

---
+++ BeerHub/ErrorAndAuthModels/HttpResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BeerHub.ErrorAndAuthModels
+{
+  public class HttpResponse : Exception
+  {
+    public HttpStatusCode StatusCode { get; set; }
+
+    public HttpResponse(HttpStatusCode statusCode, string message) : base(message)
+    {
+      StatusCode = statusCode;
+    }
+
+  }
+}
