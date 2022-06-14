8ef1af1c0a4e4a3a2d08dce8128bfd221b29f22d
JustinPerumalBBD <justinp@bbd.co.za>
2022-06-14 16:34:21 +0200

    Revert "Revert "Added Update Endpoint""
    This reverts commit 4ef09e5d4830b3930eb6183f2cde4122c405f2a1.

--- BeerHub/Controllers/CocktailController.cs
+++ BeerHub/Controllers/CocktailController.cs
@@ -14,54 +14,71 @@ namespace BeerHub.Controllers
   [Route("api")]
   public class CocktailController : ControllerBase
   {
-    private readonly DBManager _db;
+	private readonly DBManager _db;
 
-    public CocktailController(DBManager db)
-    {
-      _db = db;
-    }
+	public CocktailController(DBManager db)
+	{
+	  _db = db;
+	}
 
-    [Route("GetCocktails/{name}")]
-    [HttpGet]
-    public Cocktails GetCocktailsbyAlcohol(string name)
-    {
-      for (int j = 1; j <= _db.cocktails.Count(); j++)
-      {
-        string cocktailByAlc = _db.cocktails.Find(j).CocktailIngredients;
-        string[] words = cocktailByAlc.Split(",");
-        for (int i = 0; i < words.Length; i++)
-        {
-          if (name == words[i])
-          {
-            Cocktails cocktails = new Cocktails
-            {
-              CocktailName = _db.cocktails.Find(j).CocktailName,
-              CocktailIngredients = _db.cocktails.Find(j).CocktailIngredients,
-              Percentage = _db.cocktails.Find(j).Percentage
-            };
-            return cocktails;
-          }
-        }
-      }
-      return null;
-    }
+	[Route("GetAllCocktails/")]
+	[HttpGet]
+	public async Task<ActionResult<IEnumerable<Cocktails>>> GetAllCocktails()
+	{
+		return await _db.cocktails.ToListAsync();
+	}
 
-    [Route("GetAllCocktails/")]
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Cocktails>>> GetAllCocktails()
-    {
-      return await _db.cocktails.ToListAsync();
-    }
+	[Route("GetCocktailsByAlcohol/{name}")]
+	[HttpGet]
+	public List<Cocktails> GetCocktailsbyAlcohol(string name)
+	{
+	var cocktailList = new List<Cocktails>();
+		for (int j = 1; j <= _db.cocktails.Count(); j++)
+		{
+		string cocktailByAlc = _db.cocktails.Find(j).CocktailIngredients;
+		string[] words = cocktailByAlc.Split(",");
+		for (int i = 0; i < words.Length; i++)
+		{
+		  if (name == words[i])
+		  {
+			Cocktails cocktails = new Cocktails
+			{
+			  CocktailName = _db.cocktails.Find(j).CocktailName,
+			  CocktailIngredients = _db.cocktails.Find(j).CocktailIngredients,
+			  Percentage = _db.cocktails.Find(j).Percentage
+			};
+			cocktailList.Add(cocktails);
+		  }
+		}
+		}
+		return cocktailList;
+	}
 
-    [HttpDelete("{cocktailId}")]
-    public async Task<ActionResult> DeleteProduct(int cocktailId)
-    {
-      Cocktails cocktail = await _db.cocktails.FirstOrDefaultAsync(p => p.CocktailsId == cocktailId);
-      _db.cocktails.Remove(cocktail);
+	[Route("UpdateCocktail/{id}")]
+	[HttpPatch]
+	public async Task<ActionResult<IEnumerable<Cocktails>>> UpdateCocktail(int id, Cocktails newCocktail)
+	{
+		Cocktails cocktail = await _db.cocktails.FirstOrDefaultAsync(p => p.CocktailsId == id);
+		
+		cocktail.CocktailIngredients = newCocktail.CocktailIngredients ?? newCocktail.CocktailIngredients;
+		cocktail.CocktailName = newCocktail.CocktailName ?? newCocktail.CocktailName;
+		cocktail.Percentage = newCocktail.Percentage;
 
-      await _db.SaveChangesAsync();
+		await _db.SaveChangesAsync();
 
-      return StatusCode(201);
-    }
+			return Ok();       
+	}
+
+	[Route("DeleteCocktail/{cocktailId}")]
+	[HttpDelete]
+	public async Task<ActionResult> DeleteProduct(int cocktailId)
+	{
+	  Cocktails cocktail = await _db.cocktails.FirstOrDefaultAsync(p => p.CocktailsId == cocktailId);
+	  _db.cocktails.Remove(cocktail);
+
+	  await _db.SaveChangesAsync();
+
+	  return StatusCode(201);
+	}
   }
 }
