acbee0ba89dc1c20b68ba547d79d4719c71b1fcd
Raees Aziz Muhammad <raees@bbd.co.za>
2022-06-14 13:52:57 +0200

    Revert "Merge branch 'main' into feature/facade"
    This reverts commit 7c1abb0e589e3a98bdc628d043cf7323a12b284f, reversing
    changes made to cb74afd68601a78b67869c9a4af3d1eebec5c707.

--- BeerHub/Controllers/AlcoholController.cs
+++ BeerHub/Controllers/AlcoholController.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using BeerHub.Interfaces;
 using BeerHub.Models;
 using System.Collections.ObjectModel;
-using BeerHub.Database;
 
 namespace BeerHub.Controllers
 {
@@ -17,20 +16,12 @@ namespace BeerHub.Controllers
   [Route("api")]
   public class AlcoholController : ControllerBase
   {
-
-    private readonly DBManager _db;
-
-    public AlcoholController(DBManager db)
-    {
-      _db = db;
-    }
-
     static Main main = new Main();
 
     #region Gets
     [Route("GetAlcohol/{name}")]
     [HttpGet]
-    public Alcohols GetAlcohol(string name)
+    public Alcohol GetAlcohol(string name)
     {
       return main.GetAlcohol(name);
     }
@@ -51,17 +42,9 @@ namespace BeerHub.Controllers
 
     [Route("GetAllAlcohols/")]
     [HttpGet]
-    public Collection<Alcohols> GetAllAlcohols()
+    public List<List<Alcohol>> GetAllAlcohols()
     {
-      Console.WriteLine(_db.alcohol.Find(1).Type);
       return main.GetAllAlcohols();
-    }
-
-    [Route("GetCocktails/{name}")]
-    [HttpGet]
-    public Cocktails GetCocktails(string name)
-    {
-      return main.GetCocktails(name);
     }
 
 
